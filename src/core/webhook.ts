30ae9ec000096893ad0ad0b87abf9e126fce433b
Somto Ezechi <n.ezechi@genesystechhub.com>
2021-02-10 14:54:16 +0100

    refactor: Made webhook.sign a public method

--- src/core/webhook.ts
+++ src/core/webhook.ts
@@ -1,14 +1,17 @@
 import crypto from "crypto";
 
 export default class Webhooks {
-  private sign(payload: any, key: string): string {
-    const hash = crypto.createHmac("sha1", key).update(payload).digest("hex");
+  sign(payload: any, key: string): string {
+    const hash = crypto
+      .createHmac("sha1", key)
+      .update(JSON.stringify(payload))
+      .digest("hex");
 
     return hash;
   }
 
   verify(body: any, webhook_token: string, webhook_signature: string): Boolean {
-    const signature = this.sign(JSON.stringify(body), webhook_token);
+    const signature = this.sign(body, webhook_token);
     return signature === webhook_signature;
   }
 }
